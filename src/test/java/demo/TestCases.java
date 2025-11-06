c9a7e69914788348c4dbe06f7dc873bd4687ed75
Jyoti K <karchijyoti@gmail.com>
2025-11-06 16:17:32 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -6,12 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.testng.Reporter;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
+
 import demo.wrappers.Wrappers;
 
 public class TestCases {
@@ -21,7 +22,43 @@ public class TestCases {
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
+     @Test
+     public void testCase01() throws InterruptedException {
+        // Step 1: Launch the Google Form
+      System.out.println("Launching Google Form...");
+        driver.get("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+      
+        Wrappers obj = new Wrappers(driver);
+        
 
+        System.out.println("Filling basic details...");
+       // Step 2: Fill in the basic details
+        obj.entertext(driver.findElement(By.xpath("(//input[@type='text'])[1]")), "Crio Learner");
+        long epoch = System.currentTimeMillis()/1000;
+        obj.entertext(driver.findElement(By.xpath("//span[text()='Why are you practicing Automation?']/parent::div/parent::div/parent::div/parent::div//textarea")), "I want to be the best QA Engineer! "+Long.toString(epoch));
+        obj.selectradio(driver.findElement(By.xpath("//div[@data-value='6 - 10']")));
+       // Selecting multiple checkboxes
+        obj.selectcheckbox("Java");
+        obj.selectcheckbox("Selenium");
+        obj.selectcheckbox("TestNG");
+        // Step 3: Handle dropdown selection
+        obj.dropdownbytext(driver.findElement(By.xpath("//span[text()='Choose']")), "Mrs");
+        // Step 4: Enter date and time
+        obj.entertext(driver.findElement(By.xpath("//input[@type='date']")), obj.getCurrentDateTime("dd-MM-yyyy").toString()); 
+        obj.entertext(driver.findElement(By.xpath("//div[text()='Time']/parent::div/parent::div/parent::div//input[1]")), "07");
+        obj.entertext(driver.findElement(By.xpath("(//div[text()='Time']/parent::div/parent::div/parent::div//input)[2]")), "30");
+        // Step 5: Submit the form and validate submission
+        obj.clickElement(driver.findElement(By.xpath("//span[text()='Submit']")));
+        String ActualText="Thanks for your response, Automation Wizard!";
+        String ExpectedText=driver.findElement(By.xpath("//div[contains(text(),'Thanks for your response')]")).getText();
+        if(ActualText.toLowerCase().equals(ExpectedText.toLowerCase())){
+            Reporter.log("Test Case Passed: Form Submitted Successfully", true);
+        }else{
+            Reporter.log("Test Case Failed: Form not Submitted", true); 
+        }        
+    
+        System.out.println("Form submitted successfully!");
+    }  
      
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
@@ -31,8 +68,7 @@ public class TestCases {
     {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
-        // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
+       
 
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
@@ -47,6 +83,7 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(50));
     }
 
     @AfterTest
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -6,6 +6,8 @@ import java.time.format.DateTimeFormatter;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,7 +15,59 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+   
+    private WebDriver driver;
+      public Wrappers(WebDriver driver){
+        this.driver = driver;
+        
+      }
+     public void entertext(WebElement element,String Text){
+         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        try{ wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.sendKeys(Text);
+        }catch(Exception e){
+            System.out.println("Element not found to enter text: "+ e.getMessage());
+        }
+     }
+
+     public void clickElement(WebElement element){
+        element.click();
+     }
+
+     public void selectradio(WebElement element){
+        element.click();
+     }
+
+     public void selectcheckbox(String value){
+       WebElement element= driver.findElement(By.xpath("//span[text()='"+value+"']/parent::div/parent::div/preceding-sibling::div"));
+        element.click();
+     }
+
+     public void dropdownbytext(WebElement element, String text){
+        
+        
+         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+        System.out.println("Dropdown clicked successfully.");
+       
+        By optionLocator = By.xpath("//div[@class='OA0qNb ncFHed QXL7Te']//span[text()='"+text+"']");
+         wait.until(ExpectedConditions.visibilityOfElementLocated(optionLocator));
+         WebElement option=driver.findElement(optionLocator);
+          ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", option);
+          wait.until(ExpectedConditions.elementToBeClickable(option)).click();
+          System.out.println("Option selected successfully: "+text);
+          
+        
+    }
+
+     public String getCurrentDateTime(String format){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(format);
+        LocalDateTime now = LocalDateTime.now().minusDays(7);
+        return dtf.format(now);
+     }
+
+     
+     
+
+
 }
